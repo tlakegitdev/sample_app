c8973f99e3d01f864b821a8726a3f29f6e2c959b
Tom Lake <laket80@hotmail.com>
2013-01-24 11:05:19 -0600

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,20 +4,25 @@ describe "Static Pages" do
   
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title" do
-       visit '/static_pages/home'
-       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Home")
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
   end
 
- describe "Help page"
+ describe "Help page" do
 
     it "should have the content 'Help'" do
     	visit '/static_pages/help'
@@ -34,14 +39,17 @@ describe "Static Pages" do
 
  describe "About Page" do
 
- 	it "should have the content 'About Us'" do
- 		visit '/static_pages/about'
- 		page.should have_content('About Us')
- 	end
+ 	  it "should have the content 'About Us'" do
+ 		 visit '/static_pages/about'
+ 		 page.should have_content('About Us')
+ 	  end
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
- end
+
+  end
+
+end
